806b0ea722121b2093e60e7fae1f608889875f35
Vera Nikalaikina <vera.nikalaikina@vid.io>
2023-07-27 15:34:28 +0100

    Log http4s client

--- src/main/scala/com/github/nikalaikina/GameClient.scala
+++ src/main/scala/com/github/nikalaikina/GameClient.scala
@@ -1,24 +1,34 @@
 package com.github.nikalaikina
 
-import cats.effect.Concurrent
+import cats.effect.kernel.Async
+import cats.effect.{Concurrent, IO}
 import cats.implicits.*
 import com.github.nikalaikina.Contract.{Payment, Procurement}
 import com.github.nikalaikina.GameClient.Resp
-import com.github.nikalaikina.Ids._
-import io.circe.Json
-import org.http4s.implicits.uri
+import com.github.nikalaikina.Ids.*
+import io.circe.{Decoder, Json}
+import io.circe.syntax.*
 import org.http4s.*
 import org.http4s.client.Client
 import org.http4s.headers.Authorization
-import io.circe.Decoder
-import io.circe.syntax.*
+import org.http4s.implicits.uri
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 class GameClient[F[_]: Concurrent](client: HttpClient[F]) {
-  import io.circe.generic.auto._
+  import io.circe.generic.auto.*
 
-  def printHello: F[List[Procurement]] = {
-    client.get[Resp[List[Procurement]]](uri"https://api.spacetraders.io/v2/my/contracts")
+  def getContracts: F[List[Procurement]] = {
+    val url = uri"https://api.spacetraders.io/v2/my/contracts"
+    client
+      .run[Resp[List[Procurement]]](Request(uri = url))
       .map(_.data)
+  }
+
+  def acceptContract(id: Tag[Contract]): F[Unit] = {
+    val url = uri"https://api.spacetraders.io/v2/my/contracts" / id / "accept"
+    client
+      .run[Unit](Request(method = Method.POST, uri = url))
   }
 
 }
@@ -28,15 +38,21 @@ object GameClient {
   case class Resp[T](data: T, meta: Meta)
 }
 
-class HttpClient[F[_]: Concurrent](client: Client[F], token: String) {
+class HttpClient[F[_]: Async](client: Client[F], token: String) {
+  import org.http4s.circe.CirceEntityDecoder.circeEntityDecoder
 
-  def get[T: Decoder](uri: Uri): F[T] = {
-    import org.http4s.circe.CirceEntityDecoder.circeEntityDecoder
-    val req = Request(
-      uri = uri,
-      headers =
-        Headers(Authorization(Credentials.Token(AuthScheme.Bearer, token)))
-    )
-    client.expect[T](req)
+  private implicit val logger: Logger[F] = Slf4jLogger.getLogger[F]
+
+  val loggingClient = org.http4s.client.middleware.Logger.apply[F](
+    logHeaders = false,
+    logBody = true,
+    logAction = Some(logger.info(_))
+  )(client)
+
+  private val auth = Authorization(Credentials.Token(AuthScheme.Bearer, token))
+
+  def run[T: Decoder](req: Request[F]): F[T] = {
+    loggingClient.expect[T](req.withHeaders(Headers(auth)))
   }
+
 }
--- src/main/scala/com/github/nikalaikina/Main.scala
+++ src/main/scala/com/github/nikalaikina/Main.scala
@@ -1,12 +1,14 @@
 package com.github.nikalaikina
 
+import cats.Monad
 import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits.*
 import io.circe.Json
+import org.http4s.*
 import org.http4s.blaze.client.BlazeClientBuilder
 import org.http4s.client.Client
 import org.http4s.headers.Authorization
 import org.http4s.implicits.uri
-import org.http4s._
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import org.typelevel.log4cats.{Logger, LoggerFactory}
 
@@ -17,15 +19,19 @@ object Main extends IOApp {
   val Token = Option(System.getenv().get("TOKEN"))
     .getOrElse(throw new Exception("No TOKEN"))
 
-  override def run(
-      args: List[String]
-  ): IO[ExitCode] = {
+  override def run(args: List[String]): IO[ExitCode] = {
     BlazeClientBuilder[IO].resource
       .map(new HttpClient[IO](_, Token))
       .map(new GameClient[IO](_))
-      .use { client => client.printHello }
-      .map(println(_))
+      .use(program)
       .as(ExitCode.Success)
+  }
+
+  def program[F[_]: Monad](game: GameClient[F]): F[Unit] = {
+    for {
+      contracts <- game.getContracts
+      _ <- game.acceptContract(contracts.head.id)
+    } yield ()
   }
 
 }
